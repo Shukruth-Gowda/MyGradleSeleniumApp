bbab4c62a19b832c2ef8ba80dec8b21fcaf0b15a
Shukruth-Gowda <gshukruth@gmail.com>
2026-04-16 20:10:37 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,25 +1,33 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        WebDriver driver=new ChromeDriver();
+        // 1. Setup Chrome Options to reduce pop-up interference
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");       
+        options.addArguments("--no-sandbox");         
+        options.addArguments("--disable-dev-shm-usage"); 
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
     }
-    
     public String getGreeting() {
         return "Hello world.";
     }
+   
 }
-
